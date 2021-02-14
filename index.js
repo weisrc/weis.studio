a2113ced29d0b35f819b052b3c44389201da9bfa
STR1Z <weihengjiang64@hotmail.com>
2021-02-14 16:38:39 -0500

    animations

---
+++ index.js
@@ -0,0 +1,6 @@
+const scrollDown = document.getElementById("scroll-down");
+
+addEventListener("scroll", () => {
+  if (scrollY == 0) scrollDown.style.display = "initial";
+  else scrollDown.style.display = "none";
+});
