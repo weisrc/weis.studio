1a7add292148094a5c8add9b0bf2375e41757847
STR1Z <weihengjiang64@hotmail.com>
2021-02-15 08:09:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scrollDown = document.getElementById("scroll-down");
 
 addEventListener("scroll", () => {
-  if (scrollY == 0) scrollDown.style.display = "initial";
+  if (scrollY <= 0) scrollDown.style.display = "initial";
   else scrollDown.style.display = "none";
 });
